f496692ab0ea0aa47261788aa2a48be811a666c3
Hamza Iddaoui <hiddaoui@sqli.com>
2024-07-14 16:05:35 +0100

    [TransactionScript] introduce concrete class CalculateRevenueRecognitionsTS

--- Transaction_Script/src/test/java/org/transactionscript/TransactionScript.java
+++ Transaction_Script/src/test/java/org/transactionscript/TransactionScript.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 import org.junit.jupiter.api.Test;
+import org.transactionscript.domain.CalculateRevenueRecognitionsTS;
 import org.transactionscript.domain.RecognizedRevenueTS;
 
 import java.util.Date;
@@ -15,5 +16,12 @@ public class TransactionScript {
         RecognizedRevenueTS recognizedRevenueTS = new RecognizedRevenueTS(12L, new Date());
 
         assertThat(recognizedRevenueTS.run()).isEqualTo(true);
+    }
+
+    @Test
+    void should_run_calculate_revenue_recognitions_ts(){
+        CalculateRevenueRecognitionsTS calculateRevenueRecognitionsTS = new CalculateRevenueRecognitionsTS(12L);
+
+        assertThat(calculateRevenueRecognitionsTS.run()).isEqualTo(true);
     }
 }
