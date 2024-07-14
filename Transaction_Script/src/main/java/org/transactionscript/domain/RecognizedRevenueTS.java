aaf7e6720a9eb98db09569d91e535a7841712ea3
Hamza Iddaoui <hiddaoui@sqli.com>
2024-07-14 15:51:46 +0100

    [TransactionScript] introduce Abstract TransactionScript class and first concrete RecognizedRevenueTS class

---
+++ Transaction_Script/src/main/java/org/transactionscript/domain/RecognizedRevenueTS.java
@@ -0,0 +1,18 @@
+package org.transactionscript.domain;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+
+import java.util.Date;
+
+@Value
+@RequiredArgsConstructor
+public class RecognizedRevenueTS extends TransactionScript {
+    final Long contractNumber;
+    final Date date;
+
+    @Override
+    public boolean run() {
+        return true;
+    }
+}
